68d5eb7543d5be43ec979eb26af97eb74db56d5d
Jaiden Gerig <jaideng123@yahoo.com>
2015-06-18 14:59:23 -0500

    Table now resizes for mobile

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,3 +1,28 @@
-$("tr[data-link]").click(function() {
-  window.location = $(this).data("link")
+var show_date = true;
+var show_guard = true;
+$( window ).resize(function() {
+   if($( window ).width() < 630 && show_date){
+	   console.log("Cut Date");
+	   $('td:nth-child(8)').hide();
+	   $('th:nth-child(8)').hide();
+	   show_date = false;
+   }
+   if($( window ).width() < 530 && show_guard){
+	   console.log("Cut Guard");
+	   $('td:nth-child(7)').hide();
+	   $('th:nth-child(7)').hide();
+	   show_guard = false;
+   }
+   if($( window ).width() > 630 && !show_date){
+	   console.log("Show Date");
+	   $('td:nth-child(8)').show();
+	   $('th:nth-child(8)').show();
+	   show_date = true;
+   }
+   if($( window ).width() > 530 && !show_guard){
+	   console.log("Show Guard");
+	   $('td:nth-child(7)').show();
+	   $('th:nth-child(7)').show();
+	   show_guard = true;
+   }
 });
