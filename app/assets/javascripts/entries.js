789156e32758560523732375f6a0097e4fda240d
Jaiden Gerig <jaideng123@yahoo.com>
2015-06-18 15:10:54 -0500

    Fixed case where VP is already small enough to be scalde

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,5 +1,18 @@
+$(document).ready(function(){
 var show_date = true;
 var show_guard = true;
+if($( window ).width() < 630 && show_date){
+	   console.log("Cut Date");
+	   $('td:nth-child(8)').hide();
+	   $('th:nth-child(8)').hide();
+	   show_date = false;
+   }
+if($( window ).width() < 530 && show_guard){
+	   console.log("Cut Guard");
+	   $('td:nth-child(7)').hide();
+	   $('th:nth-child(7)').hide();
+	   show_guard = false;
+}
 $( window ).resize(function() {
    if($( window ).width() < 630 && show_date){
 	   console.log("Cut Date");
@@ -25,4 +38,5 @@ $( window ).resize(function() {
 	   $('th:nth-child(7)').show();
 	   show_guard = true;
    }
+});
 });
