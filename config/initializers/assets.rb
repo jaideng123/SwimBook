6149506e2e8c3bd78223ec2a1ac217490495472a
Jaiden Gerig <jaideng123@yahoo.com>
2015-06-20 11:24:44 -0500

    Added responsive files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( entries.js )
 Rails.application.config.assets.precompile += %w( pagination.js )
+Rails.application.config.assets.precompile += %w( dataTables.responsive.js )
