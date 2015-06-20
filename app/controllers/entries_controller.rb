03fc078971a4df8b48a6bd25d8ffa717b41f414d
Jaiden Gerig <jaideng123@yahoo.com>
2015-06-20 14:33:09 -0500

    Update entries_controller.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -64,7 +64,7 @@ class EntriesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_idea
+    def set_entry
       @entry = Entry.find(params[:id])
     end
 
