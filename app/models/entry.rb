0ad9854f5aa6323ebd59ae7ce25615ae39168349
Jaiden Gerig <jaideng123@yahoo.com>
2015-06-23 23:03:26 -0500

    Formatting changes to new_entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,11 @@
 class Entry < ActiveRecord::Base
+	validates :lname, presence: true
+	validates :fname, presence: true
+	validates :age, presence: true
+	validate :pink_or_yellow
+	def pink_or_yellow
+	  	if self.pink == false && self.yellow = false
+	   	 errors.add(:pink,"Please select a test type")
+	 	end
+	end   
 end
