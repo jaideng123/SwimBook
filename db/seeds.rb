6ffcb42cd27769e48a8fc16d425764bc79750289
Jaiden Gerig <jaideng123@yahoo.com>
2015-07-08 10:11:40 -0500

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# seeds.rb
+require 'csv'
+
+CSV.foreach("db/csv/cleaned-names.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  if row[7].present?
+      date = row[7].split("/")
+      if date[2] && date[2].length == 2
+      	date[2] = "20" + date[2]
+      end
+      row[7] = "#{date[2]}/#{date[0]}/#{date[1]}"
+   end
+   Entry.create(row.to_hash)
+end
