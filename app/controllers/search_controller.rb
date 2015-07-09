acf074b163adbcaa8993fe52b632d2fcba13bd63
Jaiden Gerig <jaideng123@yahoo.com>
2015-07-09 12:53:19 -0500

    Removed old pagination code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
 	def index
-		@entries = Entry.fuzzy_search({lname: params[:q], fname: params[:q]}, false).paginate(:page => params[:page], :per_page => 5)
+		@entries = Entry.fuzzy_search({lname: params[:q], fname: params[:q]}, false)
   	end
 end
